7e7aedcbf5becd0c2146f8ce4026658876ab19ef
Edgar-La <laraae2018@licifug.ugto.mx>
2020-07-07 18:12:53 -0500

    Algorithm complete

--- Equations_systems/Gauss_Jordan.py
+++ Equations_systems/Gauss_Jordan.py
@@ -30,7 +30,10 @@ Column_Matrix = np.array(Column_Matrix)
 Column_Matrix = np.array([[2.0],[14.0],[54.0]])
 
 os.system('clear')
-print(Matrix); print('\n'); print(Column_Matrix); input('\nClick enter to calculate')
+print(Matrix); print('\n'); print(Column_Matrix);
+Steps_option = input('\nTo shows steps type \'Yes\' otherwise  click \'enter\':\n')
+input('\nClick enter to calculate')
+os.system('clear')
 
 Expanded_Matrix = []
 for t in range (Rows):
@@ -38,28 +41,21 @@ for t in range (Rows):
 	vector_aux = np.append(Matrix[t], Column_Matrix[t])
 	Expanded_Matrix.append(vector_aux)
 Expanded_Matrix = np.array(Expanded_Matrix)
-	#print(Expanded_Matrix[t])
-
-print('---------------------------\n');print(Expanded_Matrix); print('---------------------------\n')
+print('Expanded Matrix')
+print('-------------------------------\n');print(Expanded_Matrix); print('-------------------------------\n')
 
 for t in range(Rows):
 	if Expanded_Matrix[t][t] != 1:
 		Aux = Expanded_Matrix[t][t]
 		for o in range(Columns+1):
 			Expanded_Matrix[t][o] /= Aux
-		print('\n---------------------------------\n')
-		print(Expanded_Matrix); print('\n')
+		if Steps_option == 'Yes': print(Expanded_Matrix); print('\n')
 
 	for n in range(Rows):
 		if n != t:
-			# M(n,:)=-M(n,i).*M(i,:)+M(n,:)
-			# M(n,:)=-M(n,t).*M(t,:)+M(n,:)
-			#Aux2 = Expanded_Matrix[n][o]
 			Aux2 = Expanded_Matrix[n][t]
 			for o in range(Columns+1):
 				Expanded_Matrix[n][o] = -Aux2*Expanded_Matrix[t][o]+Expanded_Matrix[n][o]#Aux2
-			print('\n---------------**------------------\n')
-			print(Expanded_Matrix); print('\n')
-
-print('---------------------------------------------------\n');print(Expanded_Matrix); print('---------------------------------------------------\n')
-#print(range(Columns+1))
+			if Steps_option == 'Yes': print(Expanded_Matrix); print('\n')
+print('Gauss Jordan Matrix')
+print('-------------------------------\n');print(Expanded_Matrix); print('-------------------------------\n')
